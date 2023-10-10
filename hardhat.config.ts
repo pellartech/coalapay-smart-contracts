ebdaa8b6d231a1db2512c442daba9a1db78ca4dc
Shayne Brady <hello@shaynebrady.com>
2023-10-11 07:38:02 +1100

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import { config } from 'dotenv'
+config()
+const hardhatConfig: HardhatUserConfig = {
+  solidity: '0.8.20',
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_PROVIDER_URL,
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  },
+  gasReporter: {
+    enabled: true
+  }
+}
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.20",
-};
-
-export default config;
+export default hardhatConfig
