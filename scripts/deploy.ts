24da59e5080e6442f40b0d74e1a021c8c7779266
Shayne Brady <hello@shaynebrady.com>
2024-08-20 10:12:39 +1000

    update deploy script for new contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,16 @@
 import { ethers } from "hardhat";
 
+const TOKEN_NAME = 'Coala Pay Sudan'
+const TOKEN_SYMBOL = 'COALA.SDN'
+const TOKEN_URI = 'https://coalapay.org/api/v2/metadata/sudan/'
+
 async function main() {
   const coalaPayImpl = await ethers.getContractFactory('CoalaPay')
-  const coalaPayContract = await coalaPayImpl.deploy()
+  const coalaPayContract = await coalaPayImpl.deploy(
+    TOKEN_NAME,
+    TOKEN_SYMBOL,
+    TOKEN_URI,
+  )
   const address = await coalaPayContract.getAddress()
   console.log(`Coala Pay deployed to: ${address}`)
 }
@@ -13,3 +21,5 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// npx hardhat verify --network mainnet 0x431B1B80f19B392EA7064fb0dBDeEbe138060fC0 "Coala Pay Sudan" "COALA.SDN" "https://coalapay.org/api/v2/metadata/sudan/"
