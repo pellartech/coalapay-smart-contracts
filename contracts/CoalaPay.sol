3246ec43528e542f382633f8e0749c99774c5600
Slymewll <slymewll@gmail.com>
2025-10-10 15:16:18 +0700

    feat: update coalapay contract with issue 10

--- contracts/CoalaPay.sol
+++ contracts/CoalaPay.sol
@@ -15,10 +15,15 @@ contract CoalaPay is ERC721, AccessControl, ReentrancyGuard {
 
     event SetTokenInfo(uint256 tokenId, TokenInfo tokenInfo);
 
-    struct TokenInfo {
+    struct Recipient {
         address receiver;
+        uint256 amount;
+    }
+
+    struct TokenInfo {
         address paymentToken;
         uint256 price;
+        Recipient[] recipients;
     }
 
     uint256 public totalSupply;
@@ -37,7 +42,9 @@ contract CoalaPay is ERC721, AccessControl, ReentrancyGuard {
         baseUri = _baseURI;
     }
 
-    function setBaseUri(string calldata _baseUri) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setBaseUri(
+        string calldata _baseUri
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         baseUri = _baseUri;
     }
 
@@ -49,20 +56,31 @@ contract CoalaPay is ERC721, AccessControl, ReentrancyGuard {
         feeTo = _feeTo;
     }
 
-    function addToken(TokenInfo calldata _tokenInfo, string calldata _projectId) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function addToken(
+        TokenInfo calldata _tokenInfo,
+        string calldata _projectId
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _validateTokenInfo(_tokenInfo);
         uint256 tokenId = totalSupply;
         tokenInfos[tokenId] = _tokenInfo;
-        totalSupply ++;
+        totalSupply++;
         emit AddTokenInfo(tokenId, _projectId, _tokenInfo);
     }
 
-    function updateToken(uint256 _tokenId, TokenInfo calldata _tokenInfo) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function updateToken(
+        uint256 _tokenId,
+        TokenInfo calldata _tokenInfo
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(totalSupply > _tokenId, "Invalid token");
+        _validateTokenInfo(_tokenInfo);
         tokenInfos[_tokenId] = _tokenInfo;
         emit SetTokenInfo(_tokenId, _tokenInfo);
     }
 
-    function updateTokenUri(uint256 _tokenId, string calldata _tokenUri) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function updateTokenUri(
+        uint256 _tokenId,
+        string calldata _tokenUri
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(totalSupply > _tokenId, "Invalid token");
         tokenUris[_tokenId] = _tokenUri;
     }
@@ -72,39 +90,64 @@ contract CoalaPay is ERC721, AccessControl, ReentrancyGuard {
         _safeMint(to, tokenId);
         TokenInfo memory _tokenInfo = tokenInfos[tokenId];
         uint256 fee = getFee(_tokenInfo.price);
-        transferPayment(_tokenInfo.receiver, _tokenInfo.paymentToken, _tokenInfo.price, fee);
+        transferPayment(_tokenInfo, fee);
     }
 
-    function adminMint(address to, uint256 tokenId) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function adminMint(
+        address to,
+        uint256 tokenId
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _safeMint(to, tokenId);
     }
 
-    function transferPayment(address to, address paymentToken, uint256 amount, uint256 fee) internal {
-        if (paymentToken == address(0)) {
-            require(amount + fee == msg.value, "Incorrect token price");
-            (bool fullAmountSuccess, ) = to.call{ value: amount }("");
-            require(fullAmountSuccess, "Transfer full amount failed");
-            (bool feeAmountSuccess, ) = feeTo.call{ value: fee }("");
+    function transferPayment(TokenInfo memory tokenInfo, uint256 fee) internal {
+        if (tokenInfo.paymentToken == address(0)) {
+            require(
+                tokenInfo.price + fee == msg.value,
+                "Incorrect token price"
+            );
+
+            (bool feeAmountSuccess, ) = feeTo.call{value: fee}("");
             require(feeAmountSuccess, "Transfer fee amount failed");
+
+            for (uint256 i = 0; i < tokenInfo.recipients.length; i++) {
+                (bool fullAmountSuccess, ) = tokenInfo
+                    .recipients[i]
+                    .receiver
+                    .call{value: tokenInfo.recipients[i].amount}("");
+                require(fullAmountSuccess, "Transfer full amount failed");
+            }
         } else {
-            SafeERC20.safeTransferFrom(IERC20(paymentToken), msg.sender, to, amount);
-            SafeERC20.safeTransferFrom(IERC20(paymentToken), msg.sender, feeTo, fee);
+            SafeERC20.safeTransferFrom(
+                IERC20(tokenInfo.paymentToken),
+                msg.sender,
+                feeTo,
+                fee
+            );
+
+            for (uint256 i = 0; i < tokenInfo.recipients.length; i++) {
+                SafeERC20.safeTransferFrom(
+                    IERC20(tokenInfo.paymentToken),
+                    msg.sender,
+                    tokenInfo.recipients[i].receiver,
+                    tokenInfo.recipients[i].amount
+                );
+            }
         }
     }
 
-    function tokenURI(uint256 _tokenId)
-        public
-        view
-        override
-        returns (string memory)
-    {
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
         if (bytes(tokenUris[_tokenId]).length > 0) {
             return tokenUris[_tokenId];
         }
         return string.concat(baseUri, _tokenId.toString());
     }
 
-    function getTokenInfo(uint256 _tokenId) public view returns (TokenInfo memory tokenInfo, uint256 fee) {
+    function getTokenInfo(
+        uint256 _tokenId
+    ) public view returns (TokenInfo memory tokenInfo, uint256 fee) {
         tokenInfo = tokenInfos[_tokenId];
         fee = getFee(tokenInfo.price);
     }
@@ -113,13 +156,21 @@ contract CoalaPay is ERC721, AccessControl, ReentrancyGuard {
         fee = (_fullAmount * feePercent) / 10000;
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, AccessControl)
-        returns (bool)
-    {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
+    function _validateTokenInfo(TokenInfo calldata _tokenInfo) internal pure {
+        require(_tokenInfo.recipients.length > 0, "Recipients are required");
+        require(_tokenInfo.price > 0, "Price is required");
+        uint256 totalAmount = 0;
+        for (uint256 i = 0; i < _tokenInfo.recipients.length; i++) {
+            require(_tokenInfo.recipients[i].amount > 0, "Recipient amount is 0");
+            require(_tokenInfo.recipients[i].receiver != address(0), "Recipient receiver is 0");
+            totalAmount += _tokenInfo.recipients[i].amount;
+        }
+        require(_tokenInfo.price == totalAmount, "Invalid price");
+    }
 }
